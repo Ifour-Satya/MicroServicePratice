4aa46166883a7e5f53aa2ae33e00ae0bd1cec2ea
Satya Prakash <ifour.satyaprakash@gmail.com>
2025-07-02 11:59:57 +0530

    change

--- MicroService.API/Product.Microservice.API/Migrations/ProductDbContextModelSnapshot.cs
+++ MicroService.API/Product.Microservice.API/Migrations/ProductDbContextModelSnapshot.cs
@@ -37,7 +37,7 @@ namespace Product.Microservice.API.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Products");
+                    b.ToTable("Products", (string)null);
                 });
 #pragma warning restore 612, 618
         }
